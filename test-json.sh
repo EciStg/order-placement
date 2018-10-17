379347f0cb87632394cc7264374e1dcd4bcea3c1
Thomas C Lindsey <tom.lindsey@gmail.com>
2018-10-16 18:07:49 -0600

    added avj to test instructions

--- test-json.sh
+++ test-json.sh
@@ -2,8 +2,8 @@
 #
 # brew install jsonlint
 # npm install jsonlint -g
-#
-
+# npm install ajv -g
+# npm install ajv-cli -v
 #
 # make sure all of the files are well-formed JSON
 find ./rsrc-schema/tst ./rsrc-schema/src -type f -name '*.json' -exec jsonlint --quiet --compact {} \;
